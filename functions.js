4ace720ed508968cbd467349e2f5dccf299b9b79
RobertBatista <robertbatista@e-get.com.br>
2024-07-10 00:08:23 -0300

    add: checkout and test nodejs

---
+++ functions.js
@@ -0,0 +1,25 @@
+/**
+ * Função de adição de dois valores
+ *
+ * @param   {number}  a  number parameter
+ * @param   {number}  b  number parameter
+ *
+ * @return  {number}     result of adiction
+ */
+const adicao = (a, b) => {
+  return a + b;
+};
+
+/**
+ * Função de subtração de dois valores
+ *
+ * @param   {number}  a  number parameter
+ * @param   {number}  b  number parameter
+ *
+ * @return  {number}     result of subtraction
+ */
+const subtracao = (a, b) => {
+  return a - b;
+};
+
+module.exports = { adicao, subtracao };
